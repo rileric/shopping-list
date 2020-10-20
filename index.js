c905e2357e40d86262f97cdf52ab362d753d0cfb
Eric Riley <eriley92@gmail.com>
2020-10-20 13:26:52 -0500

    refactored functions for readability

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*jshint esversion: 6 */
+
+
 // handle "add item"
 /*     <li>
         <span class="shopping-item">---ITEM NAME---</span>
@@ -13,40 +16,54 @@
 function createItemElements( itemName) {
 
     let preName = '<li><span class="shopping-item">';
-    let postName = '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
+    let postName = '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
 
     return (preName + itemName + postName); // return block of HTML to append
 }
 
+function handleAddItem() {
+  $('#js-shopping-list-form').on('submit', event => {
+
+    event.preventDefault();
+    const itemName = $(this).find('#shopping-list-entry').val();
+    console.log("ItemName = " + itemName); //TODO remove
+
+    let itemToAddString = createItemElements(itemName);
+    $('.shopping-list').append(itemToAddString);
+
+});
+}
+
 // for each item in list (look for li parent of current target-button)
 //     handle check (toggle "shopping-item__checked" class)
+function handleCheckButton() {
+  $('.shopping-item-toggle').on('click', event => {
+    console.log("Check button clicked"); // TODO remove
+    let parentListItem = $(this).parent('li');
+    let itemName = parentListItem.find('.shopping-item');
+   
+    itemName.toggle('.shopping-item__checked');
+ });
+}
+
 //     handle delete (aka remove() which also removes child elements)
+function handleDeleteButton() {
+  $('.shopping-item-delete').on('click', event => {
+
+    console.log("Delete button clicked"); // TODO remove
+    let parentListItem = $(this).parent('li');
+    parentListItem.remove();
+  });
+}
 
 $(function() {
 
     // handle "add item"
-    $('#js-shopping-list-form').on('submit', event => {
-
-        event.preventDefault();
-        const itemName = $(this).find('#shopping-list-entry').val();
-
-        let itemToAddString = createItemElements(itemName);
-        $('.shopping-list').append(itemToAddString);
-
-    })
+    handleAddItem();
 
     // handle delete button
-    $('.shopping-item-delete').on('click', event => {
-
-        let parentListItem = this.parent('li');
-        parentListItem.remove();
-    })
+    handleDeleteButton();
 
     // handle check button
-    $('.shopping-item-toggle').on('click', event => {
-       let parentListItem = this.parent('li');
-       let itemName = parentListItem.find('.shopping-item');
-      
-       itemName.toggle('.shopping-item__checked');
-    })
-})
+    handleCheckButton();
+});
